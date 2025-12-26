9a16f42093eedf9f79c259c413fef30ee013ffbd
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-12-26 17:40:01 +0100

    Abfrage Buch und Hinzufuegen neues Buch funktioniert

---
+++ src/graphql.js
@@ -0,0 +1,89 @@
+import { readFileSync }             from "fs";
+import createLogger                 from "logging";
+import { createSchema, createYoga } from "graphql-yoga";
+
+const logger = createLogger( "graphql" );
+
+
+// Schema aus separater Datei einlesen
+const schemaString  = readFileSync( "src/schema.graphql", "utf8" );
+logger.info( "GraphQL-Schema geladen." );
+
+
+const alleBuecherArray = [
+    {
+        id   : "1",
+        titel: "Der Herr der Ringe",
+        autor: "J.R.R. Tolkien",
+        jahr : 1954,
+        genre: "Fantasy"
+    },{
+        id   : "2",
+        titel: "James Bond: Casino Royale",
+        autor: "Ian Fleming",
+        jahr : 1953,
+        genre: "Thriller"
+    }, {
+        id   : "3",
+        titel: "1984",
+        autor: "George Orwell",
+        jahr : 1949,
+        genre: "Dystopie"
+    }, {
+        id   : "4",
+        titel: "Der kleine Prinz",
+        autor: "Antoine de Saint-Exupéry",
+        jahr : 1943,
+        genre: "Märchen"
+    }, {
+        id   : "5",
+        titel: "A Game of Thrones",
+        autor: "George R. R. Martin",
+        jahr : 1996,
+        genre: "Fantasy"
+    }
+];
+
+logger.info( `Es sind ${ alleBuecherArray.length } Bücher im Katalog.` );
+
+
+const resolversQuery = {
+
+      buecher: () => alleBuecherArray,
+
+      buch: ( _, {id} ) => {
+
+        logger.info( `Abfrage für Buch mit ID=${ id }.` );
+        return alleBuecherArray.find( buch => buch.id == id );        
+      }
+};
+
+const resolversMutation = {
+
+      buchHinzufuegen: ( _, { titel, autor, jahr, genre } ) => {
+
+        const neueId = String( alleBuecherArray.length + 1 );
+        const neuesBuch = {
+          id: neueId,
+          titel,
+          autor,
+          jahr,
+          genre
+        };
+
+        alleBuecherArray.push( neuesBuch );
+        logger.info( `Neues Buch hinzugefügt: ID=${ neueId }, Titel="${ titel }".` );
+
+        return neuesBuch;
+      }
+};
+
+export const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: schemaString,
+    resolvers: {
+      Query: resolversQuery,
+      Mutation: resolversMutation
+    }
+  })
+});
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import express                      from "express";
+import createLogger                 from "logging";
+
+import { yoga } from "./graphql.js";
+
+const logger = createLogger( "main" );
+
+const PORT_NUMMER = 8080;
+
+
+
+const app = express()
+
+// Yoga als Handler für /graphql verwenden (inkl. GraphiQL UI)
+app.use( "/graphql", yoga );
+
+app.use( express.static( "public" ) );
+
+app.listen( PORT_NUMMER, () => {
+  logger.info( `Server läuft auf http://localhost:${ PORT_NUMMER }` );
+});
